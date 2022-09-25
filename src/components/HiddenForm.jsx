92757e4d622aef4f234cb18110cf87354c638195
SamuelFoc <samo.sipikal@gmail.com>
2022-09-25 20:16:33 +0200

    Edit hidden form element

--- src/components/HiddenForm.jsx
+++ src/components/HiddenForm.jsx
@@ -1,25 +1,24 @@
 import "./HiddenForm.css";
-import qs from "qs";
-import Axios from "axios";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 function HiddenForm(props) {
+  const axiosPrivate = useAxiosPrivate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    var data = qs.stringify(props.formInfo.submit.data);
+    var data = JSON.stringify(props.formInfo.submit.data);
     console.log(props.formInfo.submit.data);
     var config = {
       method: props.formInfo.submit.method,
       url: props.formInfo.submit.url,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
       data: data,
     };
 
-    Axios(config)
+    axiosPrivate(config)
       .then((res) => {
-        window.location.reload();
+        props.showForm(false);
+        props.whatChanged(false);
       })
       .catch(function (error) {
         console.log(error);
@@ -30,9 +29,10 @@ function HiddenForm(props) {
     <div className="afterElementForm">
       <form>
         {/* INPUTS */}
-        {props.formInfo.inputs.map((input) => {
+        {props.formInfo.inputs.map((input, i) => {
           return (
             <input
+              key={i}
               className="form-control my-2"
               onChange={props.handleChange}
               type="text"
@@ -43,9 +43,9 @@ function HiddenForm(props) {
         })}
 
         {/* DATES */}
-        {props.formInfo.dates.map((input) => {
+        {props.formInfo.dates.map((input, i) => {
           return (
-            <div className="text-light">
+            <div key={i} className="text-light">
               <label htmlFor={input}>
                 {input[0].toUpperCase() + input.slice(1)}
               </label>
@@ -61,9 +61,10 @@ function HiddenForm(props) {
         })}
 
         {/* SELECTIONS */}
-        {props.formInfo.selections.map((selection) => {
+        {props.formInfo.selections.map((selection, i) => {
           return (
             <select
+              key={i}
               className="form-select my-2"
               name={selection.name}
               onChange={props.handleChange}
