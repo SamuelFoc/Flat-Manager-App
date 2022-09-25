2030ee30a4988f0b2b8fe44cda22ca93145bf307
SamuelFoc <samo.sipikal@gmail.com>
2022-09-25 20:21:15 +0200

    Add energies page

---
+++ src/components/EnergyCard.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./RespCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faRepeat } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+
+export default function EnergyCard(props) {
+  const checkClass = (data) => {
+    let myClass;
+
+    if (data === "CRITICAL") {
+      myClass = "-critical";
+    } else if (data === "HIGH") {
+      myClass = "-high";
+    } else if (data === "LOW") {
+      myClass = "-low";
+    } else {
+      myClass = "";
+    }
+
+    return myClass;
+  };
+
+  return (
+    <div className="respCard m-1">
+      <div className="row">
+        <div className={"col-6 respTitle" + checkClass(props.info.urgent)}>
+          <h6 className="my-2">{props.info.title}</h6>
+        </div>
+        <div className="col-6 d-flex justify-content-end">
+          {props.info.done ? (
+            <button
+              className="btn btn-outline-warning"
+              onClick={() => props.handleEdit(props.info.id, props.info.done)}
+            >
+              <FontAwesomeIcon icon={faRepeat} className="fs-6" />
+            </button>
+          ) : (
+            <button
+              className="btn btn-outline-success"
+              onClick={() => props.handleEdit(props.info.id, props.info.done)}
+            >
+              <FontAwesomeIcon icon={faCheck} className="fs-6" />
+            </button>
+          )}
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={() => props.handleDelete(props.info.id)}
+          >
+            <FontAwesomeIcon icon={faTrashAlt} />
+          </button>
+        </div>
+      </div>
+      <div className="row d-flex align-items-center">
+        <div className="col-8 respDescription text-start">
+          {props.info.description}
+        </div>
+        <div className={"col-2 respDate" + checkClass(props.info.urgent)}>
+          {new Date(props.info.deadline).toLocaleDateString()}
+        </div>
+        <div className="col-2 text-center">
+          <FontAwesomeIcon
+            icon={props.info.done ? faCheck : faXmark}
+            className={
+              props.info.done ? "respDoneIcon-check" : "respDoneIcon-cross"
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Energies.jsx
@@ -0,0 +1,144 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDroplet } from "@fortawesome/free-solid-svg-icons";
+import { faFireFlameCurved } from "@fortawesome/free-solid-svg-icons";
+import { faPlug } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import "./Energies.css";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import EnergyCard from "../components/EnergyCard";
+import HiddenForm from "../components/HiddenForm";
+
+export default function Energies() {
+  // TODO: STATES
+  const [energyData, setEnergyData] = useState();
+  const [newRecord, setNewRecord] = useState();
+  const [isShowedCreate, setIsShowedCreate] = useState();
+  const [whatChanged, setWhatChanged] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // TODO: HANDLERS
+  const showFormCreate = () => {
+    setIsShowedCreate(!isShowedCreate);
+    setWhatChanged("creation");
+  };
+
+  const handleChange = (event) => {
+    setNewRecord((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  // TODO: EFFECTS
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getEnergies = async () => {
+      try {
+        const response = await axiosPrivate.get("/energies");
+        isMounted && setEnergyData(response.data.data);
+      } catch (err) {
+        console.log(err);
+        navigate("/home", { state: { from: location }, replace: true });
+      }
+    };
+
+    getEnergies();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [axiosPrivate, location, navigate, whatChanged]);
+
+  // TODO: INFO OBJECTS
+  const formInfo = {
+    submit: {
+      url: "/energies",
+      method: "post",
+      data: newRecord,
+    },
+    inputs: ["measured"],
+    dates: ["date"],
+    selections: [{ name: "type", options: ["Electricity", "Gas", "Water"] }],
+    submitName: "CREATE RECORD",
+  };
+
+  console.log(energyData);
+
+  return (
+    <div className="">
+      <h3 className="color-light ms-3">
+        Energy Records{" "}
+        <FontAwesomeIcon
+          icon={faPlus}
+          className="config color-light ms-3 btn-custom"
+          onClick={showFormCreate}
+        />
+      </h3>
+      <div className="energyBox text-light">
+        <div className="row d-flex justify-content-center w-100">
+          <div className="col-12 col-md-8 col-xl-6 col-xxl-4 p-2">
+            <h3 className="ms-2 mb-4">
+              Water consumption{" "}
+              <FontAwesomeIcon icon={faDroplet} className="low-priority" />
+            </h3>
+            <div className="row m-2">
+              {energyData.water?.length > 0 ? (
+                <h1>s</h1>
+              ) : (
+                <h6>There are no records..</h6>
+              )}
+            </div>
+          </div>
+          <div className="col-12 col-md-8 col-xl-6 col-xxl-4 p-2 ">
+            <h3 className="ms-2 mb-4">
+              Gas consumption{" "}
+              <FontAwesomeIcon
+                icon={faFireFlameCurved}
+                className="high-priority"
+              />
+            </h3>
+            <div className="row m-2">
+              {energyData.gas?.length > 0 ? (
+                <h6>1</h6>
+              ) : (
+                <h6>There are no records..</h6>
+              )}
+            </div>
+          </div>
+          <div className="col-12 col-md-8 col-xl-6 col-xxl-4 p-2 ">
+            <h3 className="ms-2 mb-4">
+              Electricity consumption{" "}
+              <FontAwesomeIcon icon={faPlug} className="critical-priority" />
+            </h3>
+            <div className="row m-2">
+              {energyData.electricity?.length > 0 ? (
+                <h6>1</h6>
+              ) : (
+                <h6>There are no records..</h6>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* HIDDEN FORM FOR CREATING NEW RESPS*/}
+      {isShowedCreate && (
+        <HiddenForm
+          formInfo={formInfo}
+          showForm={showFormCreate}
+          handleChange={handleChange}
+          whatChanged={setWhatChanged}
+        />
+      )}
+    </div>
+  );
+}
