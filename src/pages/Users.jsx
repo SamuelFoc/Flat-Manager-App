ef5d07d9808e194155ed4fbaa0e119de6398e818
SamuelFoc <samo.sipikal@gmail.com>
2022-09-25 20:18:48 +0200

    Add users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
 import UserCard from "../components/UserCard";
 
 export default function Home() {
-  const [myData, setMyData] = React.useState([]);
+  const [users, setUsers] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  React.useEffect(() => {
-    fetch("http://localhost:4000/users/")
-      .then((res) => res.json())
-      .then((response) => {
-        setMyData(response.data);
-      });
-  }, []);
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get("/users");
+        isMounted && setUsers(response.data.data);
+      } catch (err) {
+        console.log(err);
+        navigate("/home", { state: { from: location }, replace: true });
+      }
+    };
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [axiosPrivate, location, navigate]);
 
   return (
     <div>
       <h3 className="color-light ms-3">My roommates</h3>
       <div className="usersBox mt-3 py-5">
-        {myData.map((user) => (
-          <UserCard info={user} />
-        ))}
+        {users?.length ? (
+          users.map((user, i) => <UserCard key={i} info={user} />)
+        ) : (
+          <p>No users to display</p>
+        )}
       </div>
     </div>
   );
