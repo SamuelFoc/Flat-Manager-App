73fae2ffb3fe5f07315574c6149d3eefa09364f7
SamuelFoc <samo.sipikal@gmail.com>
2022-09-27 09:20:13 +0200

    Edit base navigate to /home

--- src/admin/ProtectedRoute.jsx
+++ src/admin/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children, user }) {
   if (!user) {
-    return <Navigate to="/" />;
+    return <Navigate to="/home" />;
   }
   return children;
 }
