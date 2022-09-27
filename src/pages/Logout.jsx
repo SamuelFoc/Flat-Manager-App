11838d4f3c522cd03d84c3711d0eb650670a0b10
SamuelFoc <samo.sipikal@gmail.com>
2022-09-27 18:01:03 +0200

    Design logout page

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import useAuth from "../hooks/useAuth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowTurnUp } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Logout.css";
 
 const Logout = () => {
   const { setAuth } = useAuth();
@@ -13,8 +17,14 @@ const Logout = () => {
   }, [axiosPrivate]);
 
   return (
-    <div className="text-light">
-      <h1>Loged Out</h1>
+    <div className="text-light logoutPage">
+      <h1 className="me-2 logout-title">You've been logged out..</h1>
+      <FontAwesomeIcon className="mx-2 logout-icon" icon={faArrowTurnUp} />
+      <NavLink className="d-flex align-items-center" to="/admin">
+        <button className="btn btn-success logout-button">
+          Go to Log In..
+        </button>
+      </NavLink>
     </div>
   );
 };
