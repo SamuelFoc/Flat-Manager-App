4a34530c0f41b28b19e9dc88cbab2fceeb51e783
SamuelFoc <samo.sipikal@gmail.com>
2022-09-28 13:32:34 +0200

    Add special icon for Admin

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/UserCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUserLock } from "@fortawesome/free-solid-svg-icons";
 import { faMobile } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,23 +11,30 @@ function UserCard(props) {
     <div className="userCard p-3 my-2 color-light mx-3">
       <div className="d-flex">
         <div to="/" className="text-center m-2 me-3">
-          <FontAwesomeIcon icon={faUser} className="display-5 color-light" />
+          {props?.info?.isAdmin ? (
+            <FontAwesomeIcon
+              icon={faUserLock}
+              className="display-5 color-light"
+            />
+          ) : (
+            <FontAwesomeIcon icon={faUser} className="display-5 color-light" />
+          )}
         </div>
         <div>
           <div className="userCardInfoBox">
-            <h6 className="userCardTitle">{props.info.username}</h6>
-            <p className="userCardText">{props.info.age} years</p>
-            <p className="userCardText">Working at {props.info.work}</p>
+            <h6 className="userCardTitle">{props?.info?.username}</h6>
+            <p className="userCardText">{props?.info?.age} years</p>
+            <p className="userCardText">Working at {props?.info?.work}</p>
           </div>
         </div>
       </div>
       <div className="p-0">
         <FontAwesomeIcon icon={faMobile} className="fs-6 me-2 color-light" />
-        <strong style={{ fontSize: ".85rem" }}>{props.info.contact}</strong>
+        <strong style={{ fontSize: ".85rem" }}>{props?.info?.contact}</strong>
       </div>
       <div>
         <FontAwesomeIcon icon={faEnvelope} className="fs-6 me-2 color-light" />
-        <strong style={{ fontSize: ".85rem" }}>{props.info.email}</strong>
+        <strong style={{ fontSize: ".85rem" }}>{props?.info?.email}</strong>
       </div>
     </div>
   );
